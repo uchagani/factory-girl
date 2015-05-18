bb58093b570191008eeb971a2bd7de8298538432
Umair Chagani <umair.chagani@gmail.com>
2015-05-18 01:49:35 -0400

    added IRepository interface

---
+++ FactoryGirlCore/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoryGirlCore
+{
+    public interface IRepository<T>
+    {
+        T Save();
+    }
+}
