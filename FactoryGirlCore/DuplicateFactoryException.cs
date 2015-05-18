25018d6a8f360bba43a0dd286db390b4b9b13cb0
Umair Chagani <umair.chagani@gmail.com>
2015-05-18 01:48:31 -0400

    added DuplicateFactoryExeption

---
+++ FactoryGirlCore/DuplicateFactoryException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FactoryGirlCore
+{
+    public class DuplicateFactoryException : Exception
+    {
+        public DuplicateFactoryException()
+        {
+        }
+
+        public DuplicateFactoryException(string message)
+            : base(message)
+        {
+        }
+
+        public DuplicateFactoryException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected DuplicateFactoryException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
