4249519ca795a7c1820cff615e5829164c156528
Umair Chagani <umair.chagani@gmail.com>
2015-05-18 01:53:56 -0400

    Added test models

---
+++ FactoryGirlTests/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using FactoryGirlCore;
+
+namespace FactoryGirlTests.Models
+{
+    public class Book : IRepository<Book>
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public Category Category { get; set; }
+        public int Isbn { get; set; }
+        public bool Saved { get; private set; }
+
+        public Book Save()
+        {
+            Saved = true;
+            return this;
+        }
+    }
+
+    public enum Category
+    {
+        Business,
+        Art,
+        Travel,
+        History
+    }
+}
---
+++ FactoryGirlTests/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using FactoryGirlCore;
+namespace FactoryGirlTests.Models
+{
+    public class User : IRepository<User>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Role Role { get; set; }
+        public bool Saved { get; private set; }
+
+        public User Save()
+        {
+            Saved = true;
+            return this;
+        }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User
+    }
+}
