5331be91d0fb39609146f6a4b66ac419154a61fc
uchagai <umair.chagani@gmail.com>
2015-05-29 18:52:41 -0400

    adding new factory to test callbacks

---
+++ FactoryGirlTests/BookFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryGirlCore;
+using FactoryGirlTests.Models;
+
+namespace FactoryGirlTests
+{
+    public class BookFactory : FactoryBase<Book>
+    {
+        public override Func<Book> Define()
+        {
+            return () => new Book()
+            {
+                Category = Category.Business
+            };
+        }
+
+        public override Func<Book> AfterBuild()
+        {
+            return () =>
+            {
+                Instance.Author = "Voldemort";
+                return Instance;
+            };
+        }
+        
+    }
+}
---
+++ FactoryGirlTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using FactoryGirlCore;
+using FactoryGirlTests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryGirlTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FactoryGirl.Initialize(Assembly.GetExecutingAssembly());
+            var book = FactoryGirl.Build<Book>();
+        }
+    }
+}
